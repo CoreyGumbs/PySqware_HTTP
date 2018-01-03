4013ebd1f1a5d50cfab6f09b13e462130ae80670
Corey Gumbs <cgumbs@Coreys-MacBook-Pro-2.local>
2018-01-03 17:13:24 -0500

    [chore] Added ItmJson class and tests

--- sqware/catalog/item_json.py
+++ sqware/catalog/item_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 import json
 import requests
+from pathlib import Path
 from sqware.connect import Sq_Connect
 from sqware.catalog import get_categories
 
@@ -11,22 +12,53 @@ class ItmJson(object):
 	'''
 	Item json data
 	'''
-	def __init__(self, category_id=None, category_name=None):
+	def __init__(self, directory_path):
 		self.connect = Sq_Connect()
 		self.categories = get_categories(self.connect.location_id)
-		self.cat_id = category_id
-		self.cat_name = category_name
+		self.dir_path = directory_path
 
 	def __str__(self):
-		return '{} : {}'.format(self.cat_name, self.cat_id)
+		return '{}'.format('Square API Json Data')
 
-	def get_item_json(self):
+	def __get_item_json(self):
 		'''
 		Retrieves JSON data for all items/products in square catalog. 
 		'''
 		#gets data from square catalog endpoint
 		self.get_data = self.connect.get('/v2/catalog/list?types=item')
 		#decodes json data
-		self.json_data = self.get_data.json()
+		self.retrieved_data = self.get_data.json()
+		self.json_data = json.dumps(self.retrieved_data, sort_keys=True, indent=4)
+
 		return self.json_data
-	
+
+	def __directory_check(self, path_name):
+		'''
+		Checks if directory exists. If it doesn't creates directory in user supplied path.
+		'''
+		#set directory path
+		dir_name = Path(path_name)
+		#tests if directory exists. creates new one if it doesnt.
+		while not dir_name.exists():
+			dir_name.mkdir(parents=True, exist_ok=False)
+		else:
+			return dir_name.exists()
+
+	def __write_item_json(self, path_name, json_data):
+		self.directory = self.__directory_check(self.dir_path)
+		self.file_name = Path(str(path_name +'/items.json'))
+		print(self.directory)
+		if self.directory:
+			if not self.file_name.exists():
+				print('doesnt exist')
+			else:
+				return self.file_name.exists()
+
+
+	def get_data(self):
+		return self.__write_item_json(self.dir_path ,self.__get_item_json())
+
+ 
+
+
+
--- tests/test_catalog/test_sqware_item_json.py
+++ tests/test_catalog/test_sqware_item_json.py
@@ -4,6 +4,7 @@
 '''
 Test of the Sqware package Sq_Products Class.
 '''
+import os
 import pytest
 import requests 
 import json
@@ -17,16 +18,20 @@ class Test_Catalog_ItmJson(object):
 	def setup_class(cls):
 		cls.connect = Sq_Connect()
 		cls.products = Sq_Products()
-		cls.items = ItmJson()
+		cls.directory_path = '/Users/cgumbs/Devs/projects/pysqware_http/PySqware/sqware/catalog/json'
+		cls.items = ItmJson(
+			directory_path=cls.directory_path
+			)
 
-	def test_get_item_json(self):
+	def test_get_data(self):
 		'''
 		Test of json data from square items api.
 		'''
-		self.json_items = self.items.get_item_json()
+		self.json_items = self.items.get_data()
 
 		#test string in json data
-		assert 'objects' in self.json_items
+		#assert 'objects' in self.json_items
+		self.json_items
 
 	def test_write_json_data(self):
 		'''
