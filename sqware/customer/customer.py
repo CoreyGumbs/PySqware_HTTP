083840ca824eed5896067e366da1579ae7079c62
Corey Gumbs <cgumbs@Coreys-MacBook-Pro-2.local>
2018-01-15 15:13:21 -0500

    [chore] Created customer check method  and test

--- sqware/customer/customer.py
+++ sqware/customer/customer.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from sqware.connect import Sq_Connect
 
 
 class Sq_Customer(object):
 	'''
 	'''
 	def __init__(self, first_name, last_name, email, phone):
+		'''x
 		'''
-		'''
+		self.connect =  Sq_Connect()
 		self.first_name = first_name
 		self.last_name = last_name
 		self.email = email
 		self.phone = phone
 
 	def __str__(self):
+		pass
+
+
+	def check_customer(self, email):
+		'''
+		Checks if customer already exists in square.
+		'''
 		pass
--- tests/test_customer/test_sqware_customer.py
+++ tests/test_customer/test_sqware_customer.py
@@ -5,6 +5,8 @@
 Test of the Sqware package Sq_Customers Class.
 '''
 import pytest
+from sqware.connect import Sq_Connect
+from sqware.customer import Sq_Customer
 
 class Test_Customer(object):
 	'''
@@ -13,4 +15,18 @@ class Test_Customer(object):
 	def setup_class(cls):
 		'''
 		'''
-		pass
+		cls.connect = Sq_Connect()
+		cls.customer = Sq_Customer(
+			first_name = 'Testy',
+			last_name = 'McTesty',
+			email = 'McTesty@testy.com',
+			phone= '123-456-7890'
+			)
+
+	def test_def_check_for_customer(self):
+		'''
+		Checks if customer account already exists.
+		'''
+		self.customer_exists = self.customer.check_customer(self.customer.email)
+
+		assert self.customer_exists == True
